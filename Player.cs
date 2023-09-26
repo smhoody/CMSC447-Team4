7d38a302470c01cacc8fef0f4706fe52a43908fd
Steven Hoodikoff <76929918+smhoody@users.noreply.github.com>
2023-09-26 17:11:56 -0400

    Jumping animations fixed

--- Player.cs
+++ Player.cs
@@ -5,10 +5,11 @@ public class Player : KinematicBody2D
 {
     [Export] public bool right = true;
     [Export] public int speed = 230;
+    [Export] public int dash_speed = 15;
     [Export] public float gravity = 9.81f;
-    [Export] public float jump_power = 650f;
+    [Export] public float jump_power = 500f;
     [Export] public float mass = 2f;
-    [Export] public bool isJumping = false;
+    [Export] public float acceleration = 2f;
 
     // member variables here
     public Vector2 velocity = new Vector2();
@@ -29,6 +30,7 @@ public class Player : KinematicBody2D
     public override void _Process(float delta) {
         velocity.y += gravity*mass;
         GetInput();
+        CheckAnimations(delta);
         velocity = MoveAndSlide(velocity);
         velocity.x = 0;
     }
@@ -39,31 +41,46 @@ public class Player : KinematicBody2D
 
     public void GetInput() {
 
-        if (Input.IsActionJustPressed("jump")) {
-            velocity.y -= jump_power;
-        }
         if (Input.IsActionPressed("right")) {
             _animatedSprite.FlipH = false;
-            _animatedSprite.Play("run");
             velocity.x += 1;
         }
-        else if (Input.IsActionPressed("left")) {
+        else if (Input.IsActionPressed("left")) { 
             _animatedSprite.FlipH = true;
-            _animatedSprite.Play("run");
             velocity.x -= 1;
         }
-        else {
-            _animatedSprite.Stop();
-            _animatedSprite.Frame = 0;
-        }
         
+        if (groundray.IsColliding()) {
+            if (Input.IsActionJustPressed("jump")) {
+                velocity.y -= jump_power;
+            }
+        }
+
+        if (Input.IsActionJustPressed("dash") && Input.IsActionPressed("right")) {
+            velocity.x += dash_speed;
+        } 
+        else if (Input.IsActionJustPressed("dash") && Input.IsActionPressed("left")) {
+            velocity.x -= dash_speed;
+        }
+
+
         velocity.x = velocity.x * speed;
     }
 
     public void CheckAnimations(float delta) {
-        if (!groundray.IsColliding() && !isJumping) {
-            isJumping = true;
+        if (!groundray.IsColliding()) {
             _animatedSprite.Play("jump");
+        } else {
+            if (Input.IsActionPressed("right")) {
+                _animatedSprite.Play("run");
+            }
+            else if (Input.IsActionPressed("left")) {
+                _animatedSprite.Play("run");
+            }
+            else {
+                _animatedSprite.Stop();
+                _animatedSprite.Frame = 0;
+            }
         }
 
     }
